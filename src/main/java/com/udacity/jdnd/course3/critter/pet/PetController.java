ddfe0e1eee8d92a3f40e06b12641cc36f6144478
bennysun <bennysun.dev@gmail.com>
2024-05-23 21:56:22 +0800

    feat: create pet controller layer

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,12 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.pet.entities.Pet;
+import com.udacity.jdnd.course3.critter.pet.services.PetService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Pets.
@@ -11,14 +15,22 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        Pet savedPet = petService.save(convertPetDtoToEntity(petDTO), petDTO.getOwnerId());
+        return convertEntityToPetDto(savedPet);
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        Pet pet = petService.findByPetId(petId);
+        return convertEntityToPetDto(pet);
     }
 
     @GetMapping
@@ -28,6 +40,20 @@ public class PetController {
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<Pet> pets = petService.findByOwnerId(ownerId);
+        return pets.stream().map(this::convertEntityToPetDto).collect(Collectors.toList());
+    }
+
+    private Pet convertPetDtoToEntity(PetDTO petDTO) {
+        Pet pet = new Pet();
+        BeanUtils.copyProperties(petDTO, pet);
+        return pet;
+    }
+
+    private PetDTO convertEntityToPetDto(Pet pet) {
+        PetDTO petDTO = new PetDTO();
+        BeanUtils.copyProperties(pet, petDTO);
+        petDTO.setOwnerId(pet.getOwner().getId());
+        return petDTO;
     }
 }
