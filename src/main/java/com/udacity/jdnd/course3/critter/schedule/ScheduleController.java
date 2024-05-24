653c7cad5c03998b0eb4a02ae01b4bbca1ac5ef3
bennysun <bennysun.dev@gmail.com>
2024-05-24 16:50:08 +0800

    refactor: encapsulate entities into schedule service and improve test coverage

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,16 +1,12 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
 import com.udacity.jdnd.course3.critter.pet.entities.Pet;
-import com.udacity.jdnd.course3.critter.pet.services.PetService;
 import com.udacity.jdnd.course3.critter.schedule.entities.Schedule;
 import com.udacity.jdnd.course3.critter.schedule.services.ScheduleService;
-import com.udacity.jdnd.course3.critter.user.entities.Customer;
 import com.udacity.jdnd.course3.critter.user.entities.Employee;
-import com.udacity.jdnd.course3.critter.user.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,13 +18,9 @@ import java.util.stream.Collectors;
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
-    private final PetService petService;
-    private final UserService userService;
 
-    public ScheduleController(ScheduleService scheduleService, PetService petService, UserService userService) {
+    public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
-        this.petService = petService;
-        this.userService = userService;
     }
 
     @PostMapping
@@ -46,34 +38,19 @@ public class ScheduleController {
 
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
-        // todo: move to service
-        Pet pet = petService.findByPetId(petId);
-        List<Schedule> schedules = pet.getSchedules();
+        List<Schedule> schedules = scheduleService.findScheduleForPet(petId);
         return schedules.stream().map(this::convertEntityToScheduleDto).collect(Collectors.toList());
     }
 
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
-        Employee employee = userService.findEmployee(employeeId);
-        List<Schedule> schedules = employee.getSchedules();
-        // todo: move to service
-        return schedules == null
-                ? new ArrayList<>()
-                : schedules.stream().map(this::convertEntityToScheduleDto).collect(Collectors.toList());
+        List<Schedule> schedules = scheduleService.findScheduleForEmployee(employeeId);
+        return schedules.stream().map(this::convertEntityToScheduleDto).collect(Collectors.toList());
     }
 
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
-        Customer customer = userService.findCustomer(customerId);
-
-        // todo: move to service
-        List<Pet> pets = customer.getPets();
-        if (pets == null) {
-            return new ArrayList<>();
-        }
-        List<Schedule> schedules = pets.stream()
-                .flatMap(pet -> pet.getSchedules().stream())
-                .collect(Collectors.toList());
+        List<Schedule> schedules = scheduleService.findScheduleForCustomer(customerId);
         return schedules.stream().map(this::convertEntityToScheduleDto).collect(Collectors.toList());
     }
 
--- src/main/java/com/udacity/jdnd/course3/critter/schedule/services/ScheduleService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/services/ScheduleService.java
@@ -4,13 +4,16 @@ import com.udacity.jdnd.course3.critter.pet.entities.Pet;
 import com.udacity.jdnd.course3.critter.pet.repositories.PetRepository;
 import com.udacity.jdnd.course3.critter.schedule.entities.Schedule;
 import com.udacity.jdnd.course3.critter.schedule.repositories.ScheduleRepository;
+import com.udacity.jdnd.course3.critter.user.entities.Customer;
 import com.udacity.jdnd.course3.critter.user.entities.Employee;
 import com.udacity.jdnd.course3.critter.user.repositories.CustomerRepository;
 import com.udacity.jdnd.course3.critter.user.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Service
@@ -18,11 +21,13 @@ public class ScheduleService {
 
     private final PetRepository petRepository;
     private final EmployeeRepository employeeRepository;
+    private final CustomerRepository customerRepository;
     private final ScheduleRepository scheduleRepository;
 
-    public ScheduleService(PetRepository petRepository, EmployeeRepository employeeRepository, ScheduleRepository scheduleRepository, CustomerRepository customerRepository) {
+    public ScheduleService(PetRepository petRepository, EmployeeRepository employeeRepository, CustomerRepository customerRepository, ScheduleRepository scheduleRepository) {
         this.petRepository = petRepository;
         this.employeeRepository = employeeRepository;
+        this.customerRepository = customerRepository;
         this.scheduleRepository = scheduleRepository;
     }
 
@@ -36,5 +41,39 @@ public class ScheduleService {
 
     public List<Schedule> getScheduleList() {
         return scheduleRepository.findAll();
+    }
+
+    public List<Schedule> findScheduleForPet(long petId) {
+        Pet pet = petRepository.findById(petId).orElse(null);
+
+        if (pet == null) return new ArrayList<>();
+
+        return pet.getSchedules();
+    }
+
+    public List<Schedule> findScheduleForEmployee(long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElse(null);
+
+        if (employee == null) return new ArrayList<>();
+
+        List<Schedule> schedules = employee.getSchedules();
+
+        if (schedules == null) return new ArrayList<>();
+
+        return schedules;
+    }
+
+    public List<Schedule> findScheduleForCustomer(long customerId) {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+
+        if (customer == null) return new ArrayList<>();
+
+        List<Pet> pets = customer.getPets();
+
+        if (pets == null) return new ArrayList<>();
+
+        return pets.stream()
+                .flatMap(pet -> pet.getSchedules().stream())
+                .collect(Collectors.toList());
     }
 }
